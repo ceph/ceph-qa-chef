b241b8aa2f01b88e624d2e2be7bb34269ae9602c
Sandon Van Ness <sandon@inktank.com>
2013-05-11 00:44:34 -0700

    Support for architecture: ARM

--- cookbooks/ceph-qa/recipes/default.rb
+++ cookbooks/ceph-qa/recipes/default.rb
@@ -1,6 +1,6 @@
 # Check if burnupi/plana
-if !node['hostname'].match(/^(plana|burnupi|mira|vpm)/)
- raise "This recipe is only intended for plana/burnupi/mira/vpm hosts"
+if !node['hostname'].match(/^(plana|burnupi|mira|vpm|tala)/)
+ raise "This recipe is only intended for plana/burnupi/mira/vpm/tala hosts"
 end
 
 if node[:platform] == "ubuntu"
@@ -18,47 +18,51 @@ if node[:platform] == "ubuntu"
 end
 
 #Setup sources.list to use our apt mirror.
-case node[:platform]
-when "ubuntu"
-  case node[:platform_version]
-  when "12.04"
-    cookbook_file '/etc/apt/sources.list' do
-      source "sources.list.precise"
-      mode 0644
-      owner "root"
-      group "root"
-    end
-  when "12.10"
-    cookbook_file '/etc/apt/sources.list' do
-      source "sources.list.quantal"
-      mode 0644
-      owner "root"
-      group "root"
+if node[:languages][:ruby][:host_cpu] != "arm"
+  case node[:platform]
+  when "ubuntu"
+    case node[:platform_version]
+    when "12.04"
+      cookbook_file '/etc/apt/sources.list' do
+        source "sources.list.precise"
+        mode 0644
+        owner "root"
+        group "root"
+      end
+    when "12.10"
+      cookbook_file '/etc/apt/sources.list' do
+        source "sources.list.quantal"
+        mode 0644
+        owner "root"
+        group "root"
+      end
     end
   end
 end
 
-if node[:platform] == "ubuntu"
-  # for rgw
-  execute "add autobuild gpg key to apt" do
-    command <<-EOH
-  wget -q -O- 'http://ceph.com/git/?p=ceph.git;a=blob_plain;f=keys/autobuild.asc;hb=HEAD' \
-  | sudo apt-key add -
-    EOH
+if node[:languages][:ruby][:host_cpu] != "arm"
+  if node[:platform] == "ubuntu"
+    # for rgw
+    execute "add autobuild gpg key to apt" do
+      command <<-EOH
+    wget -q -O- 'http://ceph.com/git/?p=ceph.git;a=blob_plain;f=keys/autobuild.asc;hb=HEAD' \
+    | sudo apt-key add -
+      EOH
+    end
   end
-end
 
-if node[:platform] == "ubuntu"
-  # do radosgw recipe first, because it updates the apt sources and runs
-  # apt-get update for us too.
-  if node[:platform] == "ubuntu" and (node[:platform_version] == "10.10" or node[:platform_version] == "11.10" or node[:platform_version] == "12.04")
-    include_recipe "ceph-qa::radosgw"
-  else
-    Chef::Log.info("radosgw not supported on: #{node[:platform]} #{node[:platform_version]}")
+  if node[:platform] == "ubuntu"
+    # do radosgw recipe first, because it updates the apt sources and runs
+    # apt-get update for us too.
+    if node[:platform] == "ubuntu" and (node[:platform_version] == "10.10" or node[:platform_version] == "11.10" or node[:platform_version] == "12.04")
+      include_recipe "ceph-qa::radosgw"
+    else
+      Chef::Log.info("radosgw not supported on: #{node[:platform]} #{node[:platform_version]}")
   
-    # der.. well, run update.
-    execute "apt-get update" do
-      command "apt-get update"
+      # der.. well, run update.
+      execute "apt-get update" do
+        command "apt-get update"
+      end
     end
   end
 end
@@ -75,11 +79,12 @@ if node[:platform] == "ubuntu"
   # for running ceph
   package 'libedit2'
   package 'libssl0.9.8'
-
-  if node[:platform_version] == "12.10"
-    package 'libgoogle-perftools4'
-  else
-    package 'libgoogle-perftools0'
+  if node[:languages][:ruby][:host_cpu] != "arm"
+    if node[:platform_version] == "12.10"
+      package 'libgoogle-perftools4'
+    else
+      package 'libgoogle-perftools0'
+    end
   end
 
   if node[:platform_version] == "12.10"
@@ -93,8 +98,10 @@ if node[:platform] == "ubuntu"
   package 'gdisk'
   package 'parted'
 
-  # for setting BIOS settings
-  package 'smbios-utils'
+  if node[:languages][:ruby][:host_cpu] != "arm"
+    # for setting BIOS settings
+    package 'smbios-utils'
+  end
 end
 
 if node[:platform] == "centos"
@@ -262,16 +269,18 @@ if node[:platform] == "centos"
   package 'smartmontools'
 end
 
-if node[:platform] == "ubuntu"
-  file '/etc/grub.d/02_force_timeout' do
-    owner 'root'
-    group 'root'
-    mode '0755'
-    content <<-EOH
-  cat <<EOF
-  set timeout=5
-  EOF
-    EOH
+if node[:languages][:ruby][:host_cpu] != "arm"
+  if node[:platform] == "ubuntu"
+    file '/etc/grub.d/02_force_timeout' do
+      owner 'root'
+      group 'root'
+      mode '0755'
+      content <<-EOH
+    cat <<EOF
+    set timeout=5
+    EOF
+      EOH
+    end
   end
 end
 
@@ -439,96 +448,102 @@ if node[:platform] == "ubuntu"
   end
 end
 
-if node[:platform] == "ubuntu"
-  execute "enable kernel logging to console" do
-    command <<-'EOH'
-      set -e
-      f=/etc/default/grub
-      #Mira are ttyS2
-      miracheck=$(uname -n | grep -ic mira || true)
-      # if it has a setting, make sure it's to ttyS1
-      if [ $miracheck -gt 0 ]
-      then
-      if grep -q '^GRUB_CMDLINE_LINUX=.*".*console=tty0 console=ttyS[012],115200' $f; then sed 's/console=ttyS[012]/console=ttyS2/' <$f >$f.chef; fi
-      else
-      if grep -q '^GRUB_CMDLINE_LINUX=.*".*console=tty0 console=ttyS[01],115200' $f; then sed 's/console=ttyS[01]/console=ttyS1/' <$f >$f.chef; fi
-      fi
+if node[:languages][:ruby][:host_cpu] != "arm"
+  if node[:platform] == "ubuntu"
+    execute "enable kernel logging to console" do
+      command <<-'EOH'
+        set -e
+        f=/etc/default/grub
+        #Mira are ttyS2
+        miracheck=$(uname -n | grep -ic mira || true)
+        # if it has a setting, make sure it's to ttyS1
+        if [ $miracheck -gt 0 ]
+        then
+        if grep -q '^GRUB_CMDLINE_LINUX=.*".*console=tty0 console=ttyS[012],115200' $f; then sed 's/console=ttyS[012]/console=ttyS2/' <$f >$f.chef; fi
+        else
+        if grep -q '^GRUB_CMDLINE_LINUX=.*".*console=tty0 console=ttyS[01],115200' $f; then sed 's/console=ttyS[01]/console=ttyS1/' <$f >$f.chef; fi
+        fi
   
-      # if it has no setting, add it
-      if [ $miracheck -gt 0 ]
-      then
-      if ! grep -q '^GRUB_CMDLINE_LINUX=.*".* console=tty0 console=ttyS[012],115200.*' $f; then sed 's/^GRUB_CMDLINE_LINUX="\(.*\)"$/GRUB_CMDLINE_LINUX="\1 console=tty0 console=ttyS2,115200"/' <$f >$f.chef; fi
-      else
-      if ! grep -q '^GRUB_CMDLINE_LINUX=.*".* console=tty0 console=ttyS[01],115200.*' $f; then sed 's/^GRUB_CMDLINE_LINUX="\(.*\)"$/GRUB_CMDLINE_LINUX="\1 console=tty0 console=ttyS1,115200"/' <$f >$f.chef; fi
-      fi
+        # if it has no setting, add it
+        if [ $miracheck -gt 0 ]
+        then
+        if ! grep -q '^GRUB_CMDLINE_LINUX=.*".* console=tty0 console=ttyS[012],115200.*' $f; then sed 's/^GRUB_CMDLINE_LINUX="\(.*\)"$/GRUB_CMDLINE_LINUX="\1 console=tty0 console=ttyS2,115200"/' <$f >$f.chef; fi
+        else
+        if ! grep -q '^GRUB_CMDLINE_LINUX=.*".* console=tty0 console=ttyS[01],115200.*' $f; then sed 's/^GRUB_CMDLINE_LINUX="\(.*\)"$/GRUB_CMDLINE_LINUX="\1 console=tty0 console=ttyS1,115200"/' <$f >$f.chef; fi
+        fi
    
-      # if we did something; move it into place.  update-grub done below.
-      if [ -f $f.chef ] ; then mv $f.chef $f; fi
+        # if we did something; move it into place.  update-grub done below.
+        if [ -f $f.chef ] ; then mv $f.chef $f; fi
   
-      #Remove quiet kernel output:
-      sed -i 's/quiet//g' $f
-      serialcheck=$(grep -ic serial $f || true)
-      if [ $serialcheck -eq 0 ]
-      then
-      if [ $miracheck -gt 0 ]
-      then
-      echo "" >> $f
-      echo "GRUB_TERMINAL=serial" >> $f
-      echo "GRUB_SERIAL_COMMAND=\"serial --unit=2 --speed=115200 --stop=1\"" >> $f
-      else
-      echo "" >> $f
-      echo "GRUB_TERMINAL=serial" >> $f
-      echo "GRUB_SERIAL_COMMAND=\"serial --unit=1 --speed=115200 --stop=1\"" >> $f
-      fi
-      fi
+        #Remove quiet kernel output:
+        sed -i 's/quiet//g' $f
+        serialcheck=$(grep -ic serial $f || true)
+        if [ $serialcheck -eq 0 ]
+        then
+        if [ $miracheck -gt 0 ]
+        then
+        echo "" >> $f
+        echo "GRUB_TERMINAL=serial" >> $f
+        echo "GRUB_SERIAL_COMMAND=\"serial --unit=2 --speed=115200 --stop=1\"" >> $f
+        else
+        echo "" >> $f
+        echo "GRUB_TERMINAL=serial" >> $f
+        echo "GRUB_SERIAL_COMMAND=\"serial --unit=1 --speed=115200 --stop=1\"" >> $f
+        fi
+        fi
   
-      #Don't hide grub menu
+        #Don't hide grub menu
   
-      sed -i 's/^GRUB_HIDDEN_TIMEOUT.*//g' $f
+        sed -i 's/^GRUB_HIDDEN_TIMEOUT.*//g' $f
   
-      #set verbose kernel output via dmesg:
-      if ! grep -q dmesg /etc/rc.local; then sed -i 's/^exit 0/dmesg -n 7\nexit 0/g' /etc/rc.local; fi
-    EOH
+        #set verbose kernel output via dmesg:
+        if ! grep -q dmesg /etc/rc.local; then sed -i 's/^exit 0/dmesg -n 7\nexit 0/g' /etc/rc.local; fi
+      EOH
+    end
   end
 end
 
-if node[:platform] == "ubuntu"
-  execute 'update-grub' do
+if node[:languages][:ruby][:host_cpu] != "arm"
+  if node[:platform] == "ubuntu"
+    execute 'update-grub' do
+    end
   end
 end
 
-if node[:platform] == "ubuntu"
-  cookbook_file '/etc/init/ttyS1.conf' do
-     source 'ttyS1.conf'
-     mode 0644
-     owner "root"
-     group "root"
-     notifies :start, "service[ttyS1]"
-  end
-
-  if node['hostname'].match(/^(mira)/) 
-    cookbook_file '/etc/init/ttyS2.conf' do
-       source 'ttyS2.conf'
+if node[:languages][:ruby][:host_cpu] != "arm"
+  if node[:platform] == "ubuntu"
+    cookbook_file '/etc/init/ttyS1.conf' do
+       source 'ttyS1.conf'
        mode 0644
        owner "root"
        group "root"
-       notifies :start, "service[ttyS2]"
+       notifies :start, "service[ttyS1]"
     end
-  end
 
-  service "ttyS1" do
-    # Default provider for Ubuntu is Debian, and :enable doesn't work
-    # for Upstart services unless we change provider.  Assume Upstart
-    provider Chef::Provider::Service::Upstart
-    action [:enable,:start]
-  end
+    if node['hostname'].match(/^(mira)/) 
+      cookbook_file '/etc/init/ttyS2.conf' do
+         source 'ttyS2.conf'
+         mode 0644
+         owner "root"
+         group "root"
+         notifies :start, "service[ttyS2]"
+      end
+    end
 
-  if node['hostname'].match(/^(mira)/) 
-    service "ttyS2" do
+    service "ttyS1" do
       # Default provider for Ubuntu is Debian, and :enable doesn't work
       # for Upstart services unless we change provider.  Assume Upstart
       provider Chef::Provider::Service::Upstart
       action [:enable,:start]
+    end
+
+    if node['hostname'].match(/^(mira)/) 
+      service "ttyS2" do
+        # Default provider for Ubuntu is Debian, and :enable doesn't work
+        # for Upstart services unless we change provider.  Assume Upstart
+        provider Chef::Provider::Service::Upstart
+        action [:enable,:start]
+      end
     end
   end
 end
@@ -564,6 +579,7 @@ if !node['hostname'].match(/^(vpm)/)
         cidr=$(ip addr show dev eth0 | grep -iw inet | awk '{print $2}')
         ip=$(echo $cidr | cut -d'/' -f1)
         miracheck=$(uname -n | grep -ic mira)
+        armcheck=$(uname -m | grep -ic arm)
         netmask=$(ipcalc $cidr | grep -i netmask | awk '{print $2}')
         gateway=$(ipcalc $cidr | grep -i hostmin | awk '{print $2}')
         broadcast=$(ipcalc $cidr | grep -i hostmax | awk '{print $2}')
@@ -572,7 +588,12 @@ if !node['hostname'].match(/^(vpm)/)
         octet3=$(echo $ip | cut -d'.' -f3)
         octet4=$(echo $ip | cut -d'.' -f4)
         octet3=$(($octet3 + 13))
-      
+        if [ $armcheck -gt 0 ]
+        then
+        dev=eth1
+        else
+        dev=eth2
+        fi
         if [ $miracheck -gt 0 ]
         then
         sed -i "s/iface eth0 inet dhcp/\
@@ -591,8 +612,8 @@ if !node['hostname'].match(/^(vpm)/)
               gateway $gateway\n\
               broadcast $broadcast\n\
         \n\
-        auto eth2\n\
-        iface eth2 inet static\n\
+        auto $dev\n\
+        iface $dev inet static\n\
               address $octet1.$octet2.$octet3.$octet4\n\
               netmask $netmask\
         /g" /etc/network/interfaces
