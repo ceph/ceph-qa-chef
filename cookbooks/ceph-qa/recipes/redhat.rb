45a7da0c0ca51d4454b6069625f18c35ba296da1
Sandon Van Ness <sandon@inktank.com>
2013-07-25 22:23:04 -0700

    Redhat: Chef aparrantly ignores repopriority when searching.
    Revert to include version information (using the new version)
    When two versions are equal repopriority allowed chef to instal
    the correct version from the right repo but aparrantly if they are
    different versions then chef finds the newest one even fi its
    being over-ridden by a different repo due to repo priority causing
    failures.

--- cookbooks/ceph-qa/recipes/redhat.rb
+++ cookbooks/ceph-qa/recipes/redhat.rb
@@ -113,10 +113,18 @@ package 'ceph-libs' do
 end
 package 'librados2'
 package 'librbd1'
-package 'qemu-img'
-package 'qemu-kvm'
-package 'qemu-kvm-tools'
-package 'qemu-guest-agent'
+package 'qemu-img' do
+  version '0.12.1.2-2.355.el6.2.cuttlefish.async'
+end
+package 'qemu-kvm' do
+  version '0.12.1.2-2.355.el6.2.cuttlefish.async'
+end
+package 'qemu-kvm-tools' do
+  version '0.12.1.2-2.355.el6.2.cuttlefish.async'
+end
+package 'qemu-guest-agent' do
+  version '0.12.1.2-2.355.el6.2.cuttlefish.async'
+end
 package 'genisoimage'
 
 # for json_xs to investigate JSON by hand
