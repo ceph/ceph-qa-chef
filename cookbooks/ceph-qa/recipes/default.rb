34336fc1b656316f50ebd5ade6374e1ad53cd657
Sandon Van Ness <sandon@inktank.com>
2013-09-11 10:18:50 -0700

    Added support for dubia (4TB nodes).
    Signed-off-by: Sandon Van Ness <sandon@inktank.com>

--- cookbooks/ceph-qa/recipes/default.rb
+++ cookbooks/ceph-qa/recipes/default.rb
@@ -1,6 +1,6 @@
 # Check if burnupi/plana
-if !node['hostname'].match(/^(plana|burnupi|mira|vpm|tala|saya)/)
- raise "This recipe is only intended for plana/burnupi/mira/vpm/tala/saya hosts"
+if !node['hostname'].match(/^(plana|burnupi|mira|vpm|tala|saya|dubia)/)
+ raise "This recipe is only intended for plana/burnupi/mira/vpm/tala/saya/dubia hosts"
 end
 
 
