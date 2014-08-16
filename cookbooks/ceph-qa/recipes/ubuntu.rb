d63225aef96817923a5656cb50e61334a51ec1b2
Sage Weil <sage@redhat.com>
2014-08-16 15:15:46 -0700

    ubuntu: add dummy nfs export
    This is needed to make the nfs-kernel-server start on ubuntu.
    
    Signed-off-by: Sage Weil <sage@redhat.com>

--- cookbooks/ceph-qa/recipes/ubuntu.rb
+++ cookbooks/ceph-qa/recipes/ubuntu.rb
@@ -739,6 +739,28 @@ else
   end
 end
 
+# include a dummy nfs export so that the nfs kernel server starts
+file '/etc/exports' do
+ owner 'root'
+  group 'root'
+  mode '0644'
+  content <<-EOH
+# /etc/exports: the access control list for filesystems which may be exported
+#               to NFS clients.  See exports(5).
+#
+# Example for NFSv2 and NFSv3:
+# /srv/homes       hostname1(rw,sync,no_subtree_check) hostname2(ro,sync,no_subtree_check)
+#
+# Example for NFSv4:
+# /srv/nfs4        gss/krb5i(rw,sync,fsid=0,crossmnt,no_subtree_check)
+# /srv/nfs4/homes  gss/krb5i(rw,sync,no_subtree_check)
+#
+# dummy export just to make nfs_kernel_start
+/tmp    1.1.1.1(ro,sync,no_subtree_check)
+EOH
+end
+
+
 file '/ceph-qa-ready' do
   content "ok\n"
 end
