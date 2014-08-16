78c6cf4f00079edf50c376bec4cbaf528259c572
Sage Weil <sage@redhat.com>
2014-08-16 15:25:56 -0700

    sadf

--- cookbooks/ceph-qa/recipes/ubuntu.rb
+++ cookbooks/ceph-qa/recipes/ubuntu.rb
@@ -745,19 +745,19 @@ file '/etc/exports' do
   group 'root'
   mode '0644'
   content <<-EOH
-    # /etc/exports: the access control list for filesystems which may be exported
-    #               to NFS clients.  See exports(5).
-    #
-    # Example for NFSv2 and NFSv3:
-    # /srv/homes       hostname1(rw,sync,no_subtree_check) hostname2(ro,sync,no_subtree_check)
-    #
-    # Example for NFSv4:
-    # /srv/nfs4        gss/krb5i(rw,sync,fsid=0,crossmnt,no_subtree_check)
-    # /srv/nfs4/homes  gss/krb5i(rw,sync,no_subtree_check)
-    #
-    # dummy export just to make nfs_kernel_start
-    /tmp    1.1.1.1(ro,sync,no_subtree_check)
-  EOH
+# /etc/exports: the access control list for filesystems which may be exported
+#               to NFS clients.  See exports(5).
+#
+# Example for NFSv2 and NFSv3:
+# /srv/homes       hostname1(rw,sync,no_subtree_check) hostname2(ro,sync,no_subtree_check)
+#
+# Example for NFSv4:
+# /srv/nfs4        gss/krb5i(rw,sync,fsid=0,crossmnt,no_subtree_check)
+# /srv/nfs4/homes  gss/krb5i(rw,sync,no_subtree_check)
+#
+# dummy export just to make nfs_kernel_start
+/tmp    1.1.1.1(ro,sync,no_subtree_check)
+EOH
 end
 
 
